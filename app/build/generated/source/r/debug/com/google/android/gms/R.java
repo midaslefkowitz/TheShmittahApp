5b3d09a3b3aae720ad7e7a7e74dc2e0f3654fbdb
Yitz Lefkowitz <yitzlefkowitz@gmail.com>
2015-01-18 22:47:36 +0200

    Fix error in onion sefichim

--- app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -105,26 +105,26 @@ public final class R {
 		public static final int powered_by_google_light = 0x7f020098;
 	}
 	public static final class id {
-		public static final int book_now = 0x7f090022;
-		public static final int buyButton = 0x7f09001c;
-		public static final int buy_now = 0x7f090021;
-		public static final int buy_with_google = 0x7f090020;
-		public static final int classic = 0x7f090023;
-		public static final int grayscale = 0x7f090024;
-		public static final int holo_dark = 0x7f090017;
-		public static final int holo_light = 0x7f090018;
-		public static final int hybrid = 0x7f09000f;
-		public static final int match_parent = 0x7f09001e;
-		public static final int monochrome = 0x7f090025;
-		public static final int none = 0x7f090009;
-		public static final int normal = 0x7f090000;
-		public static final int production = 0x7f090019;
-		public static final int sandbox = 0x7f09001a;
-		public static final int satellite = 0x7f09000d;
-		public static final int selectionDetails = 0x7f09001d;
-		public static final int strict_sandbox = 0x7f09001b;
-		public static final int terrain = 0x7f09000e;
-		public static final int wrap_content = 0x7f09001f;
+		public static final int book_now = 0x7f0a0026;
+		public static final int buyButton = 0x7f0a0022;
+		public static final int buy_now = 0x7f0a0027;
+		public static final int buy_with_google = 0x7f0a0028;
+		public static final int classic = 0x7f0a0029;
+		public static final int grayscale = 0x7f0a002a;
+		public static final int holo_dark = 0x7f0a001d;
+		public static final int holo_light = 0x7f0a001e;
+		public static final int hybrid = 0x7f0a0013;
+		public static final int match_parent = 0x7f0a0024;
+		public static final int monochrome = 0x7f0a002b;
+		public static final int none = 0x7f0a0012;
+		public static final int normal = 0x7f0a0007;
+		public static final int production = 0x7f0a001f;
+		public static final int sandbox = 0x7f0a0020;
+		public static final int satellite = 0x7f0a0014;
+		public static final int selectionDetails = 0x7f0a0023;
+		public static final int strict_sandbox = 0x7f0a0021;
+		public static final int terrain = 0x7f0a0015;
+		public static final int wrap_content = 0x7f0a0025;
 	}
 	public static final class integer {
 		public static final int google_play_services_version = 0x7f0b0001;
